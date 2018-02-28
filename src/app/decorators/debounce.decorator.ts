bde9ceb9f1c2e81d90f5cdf02a25092d1c5502f3
Aleksandr Boldyrev <aleksandr@newsquantified.com>
2018-02-28 18:36:46 +0300

    boldyrev debounce.decorator correct

--- src/app/decorators/debounce.decorator.ts
+++ src/app/decorators/debounce.decorator.ts
@@ -1,15 +1,14 @@
-let timeout = null;
 export default function debounce(wait:number, immediate?:boolean){
-    var timeout, result;
+    let timeout, result;
     return function(target: object, key: string, value: any) {
         return {
             value: function (...args: any[]) {
-                var context = this;
-                var later = function() {
+                let context = this;
+                let later = function() {
                   timeout = null;
                   if (!immediate) result = value.value.apply(context, args);
                 };
-                var callNow = immediate && !timeout;
+                let callNow = immediate && !timeout;
                 clearTimeout(timeout);
                 timeout = setTimeout(later, wait);
                 if (callNow) result = value.value.apply(context, args);
